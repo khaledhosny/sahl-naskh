6533dc8e6695bf6d24f032752adfd3190dee5617
Khaled Hosny <khaledhosny@eglug.org>
2013-05-23 00:00:24 +0200

    Typo

--- tools/build-web.py
+++ tools/build-web.py
@@ -9,7 +9,7 @@ if len(sys.argv) > 4:
 font.version = sys.argv[3]
 # replace the full OFL with shorter text
 font.appendSFNTName ("English (US)", "License", "OFL v1.1")
-# no need for this on the wen either
+# no need for this on the web either
 font.appendSFNTName ("English (US)", "Descriptor", "")
 font.generate(sys.argv[2], flags=("round", "opentype", "short-post"))
 
